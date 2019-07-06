8d4ffd3b49f4b24d8b7fc89edd35c698ed9b2d68
odrum428 <oxkeita@gmail.com>
2019-07-06 14:40:56 +0900

    BotがSlackに投稿できるとこまで実装

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,10 @@
 import json
-from post_message_to_slack import post_message_to_slack, post_personal_message_to_slack
+from post_message_to_slack import post_message_to_slack
 
 def lambda_handler(event, context):
     print(event)
+
+    channel = event['body']['event']['channel']
+    message = "MTGを始めるよ！参加する人はjoin、興味がある人はwatchをスレッドに書き込んでね！"
+    post_message_to_slack(message, channel)
     return 0
