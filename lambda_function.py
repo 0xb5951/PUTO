c2b782482f10a3393b6798ea9c4865413c36725b
odrum428 <oxkeita@gmail.com>
2019-07-06 16:51:14 +0900

    MTG終了時にそのスレッドに対してメンションを飛ばす部分を実装

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 import json
 import threading
-from post_message_to_slack import post_message_to_slack
+from post_message_to_slack import post_message_to_slack, post_thread_message
 
 # 並列処理して重複メッセージをはじく
 def lambda_handler(event, context):
@@ -20,8 +20,7 @@ def main_func(event, context):
 
     post_text = event['body']['event']['text']
     channel = event['body']['event']['channel']
-
-    print(post_text)
+    message_ts = event['body']['event']['ts']
 
     if 'start_mtg' in post_text:
         message = "MTGを始めるよ！参加する人はjoin、興味がある人はwatchをスレッドに書き込んでね！"
@@ -30,7 +29,7 @@ def main_func(event, context):
 
     if 'end_mtg' in post_text:
         message = "MTGを終了したよ！このスレッドの内容は以下のリンクに書き込んでおいたよ！\n https://www.google.com/"
-        post_message_to_slack(message, channel)
+        res = post_thread_message(message, channel, message_ts)
         return 0
 
     message = "そんなコマンドはないよ！以下でヘルプを確認してね"
