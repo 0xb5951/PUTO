85f25b9e347bdb1ec2cfc358e2383812c0697351
odrum428 <oxkeita@gmail.com>
2019-07-06 14:19:44 +0900

    slackにパブリックなメッセージ返す関数を作成

---
+++ post_message_to_slack.py
@@ -0,0 +1,25 @@
+# Eventが発生した部屋に対して、メッセージを投げる関数
+import json
+import os
+import requests
+
+# イベントが起こった部屋にパブリックなリプライを返す
+def post_message_to_slack(message: str, channel: str):
+    # Slackのchat.postMessage APIを利用して投稿する
+
+    bot_token = "Bearer {0}".format(os.environ["SLACK_BOT_USER_ACCESS_TOKEN"])
+    oauth_token = os.environ["SLACK_OAUTH_ACCESS_TOKEN"]
+
+    url = "https://slack.com/api/chat.postMessage"
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Authorization": bot_token
+    }
+    data = {
+        "token": oauth_token,
+        "channel": channel,
+        "text": message
+    }
+
+    res = requests.post(url, data=json.dumps(data).encode("utf-8"), headers=headers)
+    return res
