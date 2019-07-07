0dfddf108a64f9bd58f72a03fa3f2227e7d5bf5c
odrum428 <oxkeita@gmail.com>
2019-07-07 11:26:50 +0900

    スレッドの発言者と内容を取得するところまで完了

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,7 @@
 import json
 import threading
+import requests
+import os
 from post_message_to_slack import post_message_to_slack, post_thread_message
 
 # 並列処理して重複メッセージをはじく
@@ -10,6 +12,27 @@ def lambda_handler(event, context):
 
 def return_200():
     return 0
+
+# そのメッセージが所属するスレッドをすべて取得
+def get_thread_messages(channel, root_ts):
+    bot_token = "Bearer {0}".format(os.environ["SLACK_BOT_USER_ACCESS_TOKEN"])
+    oauth_token = os.environ["SLACK_OAUTH_ACCESS_TOKEN"]
+
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    url = "https://slack.com/api/channels.replies?token={0}&channel={1}&thread_ts={2}".format(
+        oauth_token, channel, root_ts)
+
+    res = requests.get(url, headers=headers).json()
+    return res
+
+# Slackのユーザ一覧を取得する
+# def get_slack_user():
+#     oauth_token = os.environ["SLACK_OAUTH_ACCESS_TOKEN"]
+
+#     url = "https://slack.com/api/users.list?token={0}".format(oauth_token)
+#     res = requests.get(url).json
+#     return res
 
 def main_func(event, context):
     print(event)
@@ -28,6 +51,52 @@ def main_func(event, context):
         return 0
 
     if 'end_mtg' in post_text:
+        thread_root = event['body']['event']['thread_ts']
+        thread_messages = get_thread_messages(channel, thread_root)
+        print("thread_messages")
+        print(thread_messages)
+        print(thread_messages["messages"][0])
+        print(thread_messages["messages"][1])
+        print(thread_messages["messages"][2])
+
+
+        p_user = []
+        col_text = ''
+
+        # そのスレッドのユーザ一覧を取得する
+        url = "https://slack.com/api/users.list"
+        oauth_token = os.environ["SLACK_OAUTH_ACCESS_TOKEN"]
+
+        params = {'token': oauth_token}
+        slack_users = requests.get(url, params=params).json()
+
+        user_ids = []
+        user_names = []
+        for slack_user in slack_users["members"]:
+            user_ids.append(slack_user['id'])
+            user_names.append(slack_user['real_name'])
+
+        # スレッドからユーザが送ったメッセージのIDとテキストを抜き出す
+        for thread_message in thread_messages["messages"][1:]:
+            # ボットが送ったメッセージを無視
+            if "subtype" in thread_message:
+                if thread_message["subtype"] == 'bot_message':
+                    continue
+
+            # 参加者を抜き出す.ここは重複不可
+            if 'join' in thread_message["text"]:
+                p_user += thread_message['user']
+                continue
+
+            # 発言者情報を取得
+            if thread_message['user'] in user_ids:
+                match_point = user_ids.index(thread_message['user'])
+                col_text += "発言者 : {0}\n".format(user_names[match_point])
+
+            # 送信されたメッセージを抜き出す
+            col_text += "内容 : {0}\n\n".format(thread_message['text'])
+
+
         message = "MTGを終了したよ！このスレッドの内容は以下のリンクに書き込んでおいたよ！\n https://www.google.com/"
         res = post_thread_message(message, channel, message_ts)
         return 0
